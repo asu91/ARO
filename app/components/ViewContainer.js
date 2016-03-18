5b992c153da45041cd0db28481f02449d9742aa4
Alex Su <alexsu91@gmail.com>
2016-03-17 21:59:36 -0700

    Button centeres map on user

--- app/components/ViewContainer.js
+++ app/components/ViewContainer.js
@@ -1,9 +1,10 @@
 import React, { Component, PropTypes, View } from 'react-native';
 import AR from './AR.js';
 import Map from './Map.js';
+import Button from 'react-native-button';
 import DropNewPinButton from '../containers/container_dropNewPin'
-
 export default class ViewContainer extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -12,6 +13,7 @@ export default class ViewContainer extends Component {
         latitude: 37.78825,
         longitude: -122.4324,
       },
+      isFollowingUser: true,
     };
     this.fireRef = new Firebase("https://interruptedlobster.firebaseio.com/");
   }
@@ -22,7 +24,9 @@ export default class ViewContainer extends Component {
         var coords = {};
         coords.longitude = position.coords.longitude;
         coords.latitude = position.coords.latitude;
-        this.setState({currLoc: coords});
+        this.setState({
+          currLoc: coords
+        });
       },
       (error) => {
         alert(error.message)
@@ -44,14 +48,24 @@ export default class ViewContainer extends Component {
     );
   }
 
+
+
+        // <AR locs={ this.state.currLoc }/>
   render() {
     return (
       <View>
         <AR currLoc={ this.state.currLoc } pins={ this.props.pins.pins } />
         <Map 
           dropPin={this.props.actions.getLocationToSave}
+          initialLoc={this.state.initialLoc}
           currLoc={this.state.currLoc}
-        />
+          fullLoc={this.state.fullLoc}
+          isFollowingUser={this.state.isFollowingUser}
+          toggleFalse={
+            () => this.toggleFalse()
+          }
+        />     
+
         <DropNewPinButton/>
       </View>
     );
