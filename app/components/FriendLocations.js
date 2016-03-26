79d3a78e5a97837d979aa8440a94d2f1f9ca66f6
Alex Su <alexsu91@gmail.com>
2016-03-26 16:06:54 -0700

    working with test data

--- app/components/FriendLocations.js
+++ app/components/FriendLocations.js
@@ -1,4 +1,4 @@
-import React, { Component, Text, Image } from 'react-native';
+import React, { Component, Text, Image, View, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 import { myCurrLoc } from '../lib/db/db';
 import { PinCallout } from './PinCallout';
@@ -9,7 +9,7 @@ export default class FriendLocations extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      myLoc: {
+      coordinate: {
         latitude: 0,
         longitude: 0
       }
@@ -20,17 +20,22 @@ export default class FriendLocations extends Component {
     var self = this
     myCurrLoc.on('value', function(newLocation) {
 
-      self.setState({ myLoc: newLocation.val() });
+      self.setState({ coordinate: newLocation.val() });
     });
+  }
+
+  animate() {
+
   }
 
   render() {
     return (
+      // <View style={{borderRadius:13}}>
       <MapView.Marker
-        coordinate={this.state.myLoc}
+        coordinate={this.state.coordinate}
         image={{uri: "https://scontent.xx.fbcdn.net/hprofile-prn2/v/t1.0-1/c0.7.50.50/p50x50/993777_10151526626173598_615258953_n.jpg?oh=a23e645024f54a13baa412d052bd5a7c&oe=578DD21D"}}
         // centerOffset={{ x: -42, y: -60 }}
-        style={{borderRadius: 13, backgroundColor: 'transparent'}}
+        style={styles.icon}
       >
         <MapView.Callout tooltip>
           <PinCallout>
@@ -38,11 +43,16 @@ export default class FriendLocations extends Component {
           </PinCallout>
         </MapView.Callout>
       </MapView.Marker>
+      // </View>
 
     );
   }
-            // <Image
-            //   source={{uri: "https://scontent.xx.fbcdn.net/hprofile-prn2/v/t1.0-1/c0.7.50.50/p50x50/993777_10151526626173598_615258953_n.jpg?oh=a23e645024f54a13baa412d052bd5a7c&oe=578DD21D"}}
-            // />
-
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    borderRadius: 13,
+    backgroundColor: 'transparent',
+  }
+})
+
