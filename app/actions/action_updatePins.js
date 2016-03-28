c6b925e804e6b3055c93a8119e6bbc6df79ba924
Tiffany <tet226@nyu.edu>
2016-03-28 16:43:33 -0700

    delete unecessary actions/reducers for alert Please enter the commit message for your changes. Lines starting

--- app/actions/action_updatePins.js
+++ app/actions/action_updatePins.js
@@ -1,15 +1,9 @@
-import { UPDATE_PINS, ALERT_SHARE } from '../constants/constants.js';
+import { UPDATE_PINS } from '../constants/constants.js';
 import { userData } from '../lib/db/db.js';
 import { Alert } from 'react-native';
 const updatePins = (payload) => {
   return {
     type: UPDATE_PINS,
-    payload
-  };
-};
-const alertShare = (payload) => {
-  return {
-    type: ALERT_SHARE,
     payload
   };
 };
@@ -24,17 +18,15 @@ export default function(pin, newTitle) {
     userData.on("child_added", function(snap) {
       var pin = snap.val();
         if (pin.alertedYet !== null && pin.alertedYet === false) {
-          dispatch(alertShare(pin));
           var message = pin.friend.name + " shared a pin with you!";
-          console.log(message)
           Alert.alert(message, null,
           [
             {text: 'Show me pin!', onPress: () => console.log('Ask me later pressed')},
             {text: 'OK', onPress: () => console.log('OK Pressed')},
           ]);
-          // console.log('found pin from friend!', pin);
+          //once alerted, yet alertedYet to true so it doesn't alert again
+          userData.child(pin.id).update({alertedYet: true});
         }
-
     });
     userData.on("value", function(snap) {
       dispatch(updatePins(snap.val()));
