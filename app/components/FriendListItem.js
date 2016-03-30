7ebf6730ff083bcfb7c458311ba7cbab893ab28f
Alex Su <alexsu91@gmail.com>
2016-03-30 10:55:12 -0700

    added friend facebook photo to friend list

--- app/components/FriendListItem.js
+++ app/components/FriendListItem.js
@@ -1,4 +1,12 @@
-import React, {Component, Image, Text, TouchableHighlight, View, StyleSheet} from 'react-native';
+import React, {
+  Component,
+  Image,
+  Text,
+  TouchableHighlight,
+  View,
+  StyleSheet
+} from 'react-native';
+
 import { Actions } from 'react-native-router-flux';
 
 export default class FriendListItem extends Component {
@@ -10,6 +18,7 @@ export default class FriendListItem extends Component {
   render() {
     const { friend } = this.props;
     // const pic = friend.picture; TODO: Add picture and email
+    console.log(friend.picture,'asdfasd')
     return (
       <TouchableHighlight 
         onPress={() => {
@@ -19,6 +28,10 @@ export default class FriendListItem extends Component {
         }}
       >
         <View style={style.container}>
+          <Image
+            source={{ uri: friend.picture }}
+            style={style.picture}
+          />
           <Text style={style.text}>
             {friend.name}
           </Text>
@@ -35,6 +48,7 @@ const style = StyleSheet.create({
     borderRadius: 4,
     margin: 4,
     padding: 6,
+    flexDirection: 'row',
   },
   text: {
     alignSelf: 'center',
@@ -45,5 +59,10 @@ const style = StyleSheet.create({
     margin: 4,
     borderWidth: 1,
     borderColor: '#000000',
+  },
+  picture: {
+    height: 50,
+    width: 50,
+    alignSelf: 'flex-start',
   }
 });
